89f6595c17a765e9c6c6b7745cbf8fee4b987b9c
Daramola Kehinde Joseph <81877552+Kennypapa@users.noreply.github.com>
2024-08-28 21:07:01 +0100

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue';
-import Cart from './page/Cart.vue';
+import SectionProduct from './page/SectionProduct.vue';
 </script>
 
 <template>
   <Navbar />
-  <Cart />
+
+  <SectionProduct />
 </template>
 
 <style scoped></style>
---
+++ src/page/SectionProduct.vue
@@ -0,0 +1,124 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="w-[1250px] mx-auto flex gap-3 justify-between items-start mt-14 cartLayout">
+        <div class="w-full">
+            <ul class="flex gap-2">
+
+                <li class="flex justify-start items-center">
+
+                    <p class="text-right text-dark-text text-xs">Home page</p>
+
+                    <span class="ml-3"><svg width="6" height="9" viewBox="0 0 7 12" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 11L6 6L1 1" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </span>
+                </li>
+
+
+                <li class="flex justify-start items-center">
+                    <p class="text-right text-dark-text text-xs">Rings</p>
+
+                    <span class="ml-3"><svg width="6" height="9" viewBox="0 0 7 12" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 11L6 6L1 1" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </span>
+                </li>
+
+
+                <li class="flex justify-start items-center">
+                    <p class="text-right text-dark-text text-xs">Rings</p>
+
+                    <span class="ml-3"><svg width="6" height="9" viewBox="0 0 7 12" fill="none"
+                            xmlns="http://www.w3.org/2000/svg">
+                            <path d="M1 11L6 6L1 1" stroke="black" stroke-linecap="round" stroke-linejoin="round" />
+                        </svg>
+                    </span>
+                </li>
+
+            </ul>
+
+
+            <div class="flex justify-between items-start w-full gap-8">
+
+                <div class="w-full h-[600px] border">
+
+                </div>
+
+
+                <div class="w-[900px] border h-[500px]">
+
+                    <p class=" text-dark-text text-[28px] font-bold mb-3">
+                        Double heart sparkling ring
+                    </p>
+
+                    <p class=" text-dark-text text-base font-semibold ">
+                        €119.00
+                    </p>
+
+                    <div class="flex justify-start items-center mt-2">
+
+                        <div id="Ratings" class="flex gap-1">
+                            <span>
+                                <svg width="17" height="17" viewBox="0 0 6 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M2.85232 4.77358L4.23565 5.64831C4.48899 5.80862 4.79899 5.57164 4.73232 5.27193L4.36565 3.62701L5.58899 2.51878C5.81232 2.31665 5.69232 1.9333 5.39899 1.90891L3.78899 1.76602L3.15899 0.211714C3.04565 -0.0705712 2.65899 -0.0705712 2.54565 0.211714L1.91565 1.76254L0.305652 1.90542C0.0123192 1.92982 -0.107681 2.31317 0.115652 2.5153L1.33899 3.62353L0.972319 5.26845C0.905652 5.56816 1.21565 5.80514 1.46899 5.64483L2.85232 4.77358Z"
+                                        fill="#FFD81D" />
+                                </svg>
+                            </span>
+                            <span>
+                                <svg width="17" height="17" viewBox="0 0 6 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M2.85232 4.77358L4.23565 5.64831C4.48899 5.80862 4.79899 5.57164 4.73232 5.27193L4.36565 3.62701L5.58899 2.51878C5.81232 2.31665 5.69232 1.9333 5.39899 1.90891L3.78899 1.76602L3.15899 0.211714C3.04565 -0.0705712 2.65899 -0.0705712 2.54565 0.211714L1.91565 1.76254L0.305652 1.90542C0.0123192 1.92982 -0.107681 2.31317 0.115652 2.5153L1.33899 3.62353L0.972319 5.26845C0.905652 5.56816 1.21565 5.80514 1.46899 5.64483L2.85232 4.77358Z"
+                                        fill="#FFD81D" />
+                                </svg>
+                            </span>
+                            <span>
+                                <svg width="17" height="17" viewBox="0 0 6 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M2.85232 4.77358L4.23565 5.64831C4.48899 5.80862 4.79899 5.57164 4.73232 5.27193L4.36565 3.62701L5.58899 2.51878C5.81232 2.31665 5.69232 1.9333 5.39899 1.90891L3.78899 1.76602L3.15899 0.211714C3.04565 -0.0705712 2.65899 -0.0705712 2.54565 0.211714L1.91565 1.76254L0.305652 1.90542C0.0123192 1.92982 -0.107681 2.31317 0.115652 2.5153L1.33899 3.62353L0.972319 5.26845C0.905652 5.56816 1.21565 5.80514 1.46899 5.64483L2.85232 4.77358Z"
+                                        fill="#FFD81D" />
+                                </svg>
+                            </span>
+                            <span>
+                                <svg width="17" height="17" viewBox="0 0 6 6" fill="none"
+                                    xmlns="http://www.w3.org/2000/svg">
+                                    <path
+                                        d="M2.85232 4.77358L4.23565 5.64831C4.48899 5.80862 4.79899 5.57164 4.73232 5.27193L4.36565 3.62701L5.58899 2.51878C5.81232 2.31665 5.69232 1.9333 5.39899 1.90891L3.78899 1.76602L3.15899 0.211714C3.04565 -0.0705712 2.65899 -0.0705712 2.54565 0.211714L1.91565 1.76254L0.305652 1.90542C0.0123192 1.92982 -0.107681 2.31317 0.115652 2.5153L1.33899 3.62353L0.972319 5.26845C0.905652 5.56816 1.21565 5.80514 1.46899 5.64483L2.85232 4.77358Z"
+                                        fill="#FFD81D" />
+                                </svg>
+                            </span>
+                        </div>
+
+                        <div class="flex ml-3">
+                            <p class="text-dark-text font-light text-sm ml-1">
+                                4.1
+                            </p>
+                            <p class="text-dark-text font-light text-sm">
+                                (66)
+                            </p>
+                        </div>
+                    </div>
+
+
+                    <div class="mt-8">
+                        <p class="text-dark-text text-base font-normal">
+                            Metal: Gold plated
+                        </p>
+                    </div>
+
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped></style>
