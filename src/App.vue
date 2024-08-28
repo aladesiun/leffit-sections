f786578709b7157fef601f113a5146fdf3e85653
Aladesiun Temitope <83991100+aladesiun@users.noreply.github.com>
2024-08-28 21:58:57 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue';
 import SectionProduct from './page/SectionProduct.vue';
+import Home2 from './page/Home2.vue';
 import Cart from './page/Cart.vue';
 import Footer from './components/Footer.vue';
 </script>
