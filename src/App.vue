8840a3268339716e3add6138a0a8a271fc7ed656
Daramola Kehinde Joseph <81877552+Kennypapa@users.noreply.github.com>
2024-08-28 21:52:17 +0100

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <script setup lang="ts">
 import Navbar from './components/Navbar.vue';
 import SectionProduct from './page/SectionProduct.vue';
+import Cart from './page/Cart.vue';
+import Footer from './components/Footer.vue';
 </script>
 
 <template>
   <Navbar />
-
+  <Home />
+  <Home2 />
   <SectionProduct />
+  <Cart />
+  <Footer />
 </template>
 
 <style scoped></style>
