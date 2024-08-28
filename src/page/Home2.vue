e149ea5da083920321273a83bd55370e200e7522
Aladesiun Temitope <83991100+aladesiun@users.noreply.github.com>
2024-08-28 21:59:23 +0100

    Update Home2.vue

--- src/page/Home2.vue
+++ src/page/Home2.vue
@@ -3,10 +3,8 @@
     <BlocksProducts/>
     <SectionCheckout/>
     <UspSlider/>
-    <Footer/>
 </template>
 <script setup lang="ts">
-import Footer from '.././components/Footer.vue';
 import SectionCheckout from '.././components/SectionCheckout.vue';
 import BlocksProducts from '.././components/BlocksProducts.vue';
 import UspSlider from '../components/UspSlider.vue';
