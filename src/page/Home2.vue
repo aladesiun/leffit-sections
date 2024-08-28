5302bb47701badb13704b28ada763ea06f862d33
Aladesiun Temitope <83991100+aladesiun@users.noreply.github.com>
2024-08-28 13:57:48 +0100

    updates

--- src/page/Home2.vue
+++ src/page/Home2.vue
@@ -1,7 +1,9 @@
 
 <template>
     <BlocksProducts/>
+    <Footer/>
 </template>
 <script setup lang="ts">
+import Footer from '.././components/Footer.vue';
 import BlocksProducts from '.././components/BlocksProducts.vue';
 </script>
