7d8adced92c34550c881dd959e573f6dadb8f4eb
Daramola Kehinde Joseph <81877552+Kennypapa@users.noreply.github.com>
2024-08-29 09:12:42 +0100

    Update SectionProduct.vue

--- src/page/SectionProduct.vue
+++ src/page/SectionProduct.vue
@@ -42,16 +42,10 @@
 
             </ul>
 
-
             <div class="flex justify-between items-start w-full gap-8">
+                <div class="w-full h-[600px] border"></div>
 
-                <div class="w-full h-[600px] border">
-
-                </div>
-
-
-
-                <div id="product-details" class="w-[780px] border h-[500px]">
+                <div id="product-details" class="w-[780px] border ">
 
                     <p class=" text-dark-text text-[28px] font-bold mb-3">
                         Double heart sparkling ring
@@ -108,7 +102,6 @@
                         </div>
 
                     </div>
-
 
                     <div class="mt-8">
                         <p class="text-dark-text text-base font-normal">
@@ -222,14 +215,14 @@
 
                         <div id="Accordion">
 
-                            <div id="accordionFlushExample">
+                            <div id="accordionExample">
                                 <div
-                                    class="rounded-none border border-e-0 border-s-0 border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-body-dark">
-                                    <h2 class="mb-0" id="flush-headingOne">
+                                    class="rounded-t-lg border border-neutral-200 bg-white dark:border-neutral-600 dark:bg-body-dark">
+                                    <h2 class="mb-0" id="headingOne">
                                         <button
-                                            class="group relative flex w-full items-center rounded-none border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-body-dark dark:text-white [&:not([data-twe-collapse-collapsed])]:bg-white [&:not([data-twe-collapse-collapsed])]:text-primary [&:not([data-twe-collapse-collapsed])]:shadow-border-b dark:[&:not([data-twe-collapse-collapsed])]:bg-surface-dark dark:[&:not([data-twe-collapse-collapsed])]:text-primary dark:[&:not([data-twe-collapse-collapsed])]:shadow-white/10 "
-                                            type="button" data-twe-collapse-init data-twe-target="#flush-collapseOne"
-                                            aria-expanded="false" aria-controls="flush-collapseOne">
+                                            class="group relative flex w-full items-center rounded-t-lg border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-body-dark dark:text-white [&:not([data-twe-collapse-collapsed])]:bg-white [&:not([data-twe-collapse-collapsed])]:text-primary [&:not([data-twe-collapse-collapsed])]:shadow-border-b dark:[&:not([data-twe-collapse-collapsed])]:bg-surface-dark dark:[&:not([data-twe-collapse-collapsed])]:text-primary dark:[&:not([data-twe-collapse-collapsed])]:shadow-white/10 "
+                                            type="button" data-twe-collapse-init data-twe-target="#collapseOne"
+                                            aria-expanded="true" aria-controls="collapseOne">
                                             Accordion Item #1
                                             <span
                                                 class="-me-1 ms-auto h-5 w-5 shrink-0 rotate-[-180deg] transition-transform duration-200 ease-in-out group-data-[twe-collapse-collapsed]:me-0 group-data-[twe-collapse-collapsed]:rotate-0 motion-reduce:transition-none [&>svg]:h-6 [&>svg]:w-6">
@@ -241,28 +234,31 @@
                                             </span>
                                         </button>
                                     </h2>
-                                    <div id="flush-collapseOne" class="!visible border-0" data-twe-collapse-item
-                                        data-twe-collapse-show aria-labelledby="flush-headingOne"
-                                        data-twe-parent="#accordionFlushExample">
+                                    <div id="collapseOne" class="!visible" data-twe-collapse-item data-twe-collapse-show
+                                        aria-labelledby="headingOne" data-twe-parent="#accordionExample">
                                         <div class="px-5 py-4">
-                                            Placeholder content for this accordion, which is intended to
-                                            demonstrate the
-                                            <code>.accordion-flush</code> class. This is the first item's
-                                            accordion body.
+                                            <strong>This is the first item's accordion body.</strong> It is
+                                            shown by default, until the collapse plugin adds the appropriate
+                                            classes that we use to style each element. These classes control
+                                            the overall appearance, as well as the showing and hiding via CSS
+                                            transitions. You can modify any of this with custom CSS or
+                                            overriding our default variables. It's also worth noting that just
+                                            about any HTML can go within the <code>.accordion-body</code>,
+                                            though the transition does limit overflow.
                                         </div>
                                     </div>
                                 </div>
                                 <div
-                                    class="rounded-none border border-e-0 border-s-0 border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-body-dark">
-                                    <h2 class="mb-0" id="flush-headingTwo">
+                                    class="border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-body-dark">
+                                    <h2 class="mb-0" id="headingTwo">
                                         <button
-                                            class="group relative flex w-full items-center rounded-none border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-body-dark dark:text-white [&:not([data-twe-collapse-collapsed])]:bg-white [&:not([data-twe-collapse-collapsed])]:text-primary [&:not([data-twe-collapse-collapsed])]:shadow-border-b dark:[&:not([data-twe-collapse-collapsed])]:bg-surface-dark dark:[&:not([data-twe-collapse-collapsed])]:text-primary dark:[&:not([data-twe-collapse-collapsed])]:shadow-white/10 "
+                                            class="group relative flex w-full items-center border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-body-dark dark:text-white [&:not([data-twe-collapse-collapsed])]:bg-white [&:not([data-twe-collapse-collapsed])]:text-primary [&:not([data-twe-collapse-collapsed])]:shadow-border-b dark:[&:not([data-twe-collapse-collapsed])]:bg-surface-dark dark:[&:not([data-twe-collapse-collapsed])]:text-primary dark:[&:not([data-twe-collapse-collapsed])]:shadow-white/10 "
                                             type="button" data-twe-collapse-init data-twe-collapse-collapsed
-                                            data-twe-target="#flush-collapseTwo" aria-expanded="false"
-                                            aria-controls="flush-collapseTwo">
+                                            data-twe-target="#collapseTwo" aria-expanded="false"
+                                            aria-controls="collapseTwo">
                                             Accordion Item #2
                                             <span
-                                                class="-me-1 ms-auto h-5 w-5 shrink-0 rotate-[-180deg] transition-transform duration-200 ease-in-out group-data-[twe-collapse-collapsed]:me-0 group-data-[twe-collapse-collapsed]:rotate-0 motion-reduce:transition-none [&>svg]:h-6 [&>svg]:w-6">
+                                                class="-me-1 ms-auto h-5 w-5 shrink-0 rotate-[-180deg] transition-transform duration-200 ease-in-out group-data-[twe-collapse-collapsed]:me-0 group-data-[twe-collapse-collapsed]:rotate-0 motion-reduce:transition-none [&>svg]:h-6 [&>svg]:w-6 ">
                                                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
                                                     stroke-width="1.5" stroke="currentColor">
                                                     <path stroke-linecap="round" stroke-linejoin="round"
@@ -271,25 +267,28 @@
                                             </span>
                                         </button>
                                     </h2>
-                                    <div id="flush-collapseTwo" class="!visible hidden border-0" data-twe-collapse-item
-                                        aria-labelledby="flush-headingTwo" data-twe-parent="#accordionFlushExample">
+                                    <div id="collapseTwo" class="!visible hidden" data-twe-collapse-item
+                                        aria-labelledby="headingTwo" data-twe-parent="#accordionExample">
                                         <div class="px-5 py-4">
-                                            Placeholder content for this accordion, which is intended to
-                                            demonstrate the
-                                            <code>.accordion-flush</code> class. This is the second item's
-                                            accordion body. Let's imagine this being filled with some actual
-                                            content.
+                                            <strong>This is the second item's accordion body.</strong> It is
+                                            hidden by default, until the collapse plugin adds the appropriate
+                                            classes that we use to style each element. These classes control
+                                            the overall appearance, as well as the showing and hiding via CSS
+                                            transitions. You can modify any of this with custom CSS or
+                                            overriding our default variables. It's also worth noting that just
+                                            about any HTML can go within the <code>.accordion-body</code>,
+                                            though the transition does limit overflow.
                                         </div>
                                     </div>
                                 </div>
                                 <div
-                                    class="rounded-none border border-b-0 border-e-0 border-s-0 border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-body-dark">
-                                    <h2 class="mb-0" id="flush-headingThree">
+                                    class="rounded-b-lg border border-t-0 border-neutral-200 bg-white dark:border-neutral-600 dark:bg-body-dark">
+                                    <h2 class="accordion-header mb-0" id="headingThree">
                                         <button
-                                            class="group relative flex w-full items-center rounded-none border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none dark:bg-body-dark dark:text-white [&:not([data-twe-collapse-collapsed])]:bg-white [&:not([data-twe-collapse-collapsed])]:text-primary [&:not([data-twe-collapse-collapsed])]:shadow-border-b dark:[&:not([data-twe-collapse-collapsed])]:bg-surface-dark dark:[&:not([data-twe-collapse-collapsed])]:text-primary dark:[&:not([data-twe-collapse-collapsed])]:shadow-white/10 "
+                                            class="data-[twe-collapse-collapsed] group relative flex w-full items-center border-0 bg-white px-5 py-4 text-left text-base text-neutral-800 transition [overflow-anchor:none] hover:z-[2] focus:z-[3] focus:outline-none data-[twe-collapse-collapsed]:rounded-b-lg dark:bg-body-dark dark:text-white [&:not([data-twe-collapse-collapsed])]:bg-white [&:not([data-twe-collapse-collapsed])]:text-primary [&:not([data-twe-collapse-collapsed])]:shadow-border-b dark:[&:not([data-twe-collapse-collapsed])]:bg-surface-dark dark:[&:not([data-twe-collapse-collapsed])]:text-primary  dark:[&:not([data-twe-collapse-collapsed])]:shadow-white/10"
                                             type="button" data-twe-collapse-init data-twe-collapse-collapsed
-                                            data-twe-target="#flush-collapseThree" aria-expanded="false"
-                                            aria-controls="flush-collapseThree">
+                                            data-twe-target="#collapseThree" aria-expanded="false"
+                                            aria-controls="collapseThree">
                                             Accordion Item #3
                                             <span
                                                 class="-me-1 ms-auto h-5 w-5 shrink-0 rotate-[-180deg] transition-transform duration-200 ease-in-out group-data-[twe-collapse-collapsed]:me-0 group-data-[twe-collapse-collapsed]:rotate-0 motion-reduce:transition-none [&>svg]:h-6 [&>svg]:w-6">
@@ -301,26 +300,27 @@
                                             </span>
                                         </button>
                                     </h2>
-                                    <div id="flush-collapseThree" class="!visible hidden" data-twe-collapse-item
-                                        aria-labelledby="flush-headingThree" data-twe-parent="#accordionFlushExample">
+                                    <div id="collapseThree" class="!visible hidden" data-twe-collapse-item
+                                        aria-labelledby="headingThree" data-twe-parent="#accordionExample">
                                         <div class="px-5 py-4">
-                                            Placeholder content for this accordion, which is intended to
-                                            demonstrate the
-                                            <code>.accordion-flush</code> class. This is the third item's
-                                            accordion body. Nothing more exciting happening here in terms of
-                                            content, but just filling up the space to make it look, at least
-                                            at first glance, a bit more representative of how this would look
-                                            in a real-world application.
+                                            <strong>This is the third item's accordion body.</strong> It is
+                                            hidden by default, until the collapse plugin adds the appropriate
+                                            classes that we use to style each element. These classes control
+                                            the overall appearance, as well as the showing and hiding via CSS
+                                            transitions. You can modify any of this with custom CSS or
+                                            overriding our default variables. It's also worth noting that just
+                                            about any HTML can go within the <code>.accordion-body</code>,
+                                            though the transition does limit overflow.
                                         </div>
                                     </div>
                                 </div>
                             </div>
+
                         </div>
 
                     </div>
 
                 </div>
-
             </div>
         </div>
     </div>
